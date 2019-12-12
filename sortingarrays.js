d3b51db6308600dafd5c1af338babb1419c83a8d
kenta918 <56744380+kenta918@users.noreply.github.com>
2019-12-11 14:19:28 -1000

    files

---
+++ sortingarrays.js
@@ -0,0 +1,12 @@
+//var food =["apple","banana"];
+//var newarr = food.toString();
+//window.alert(newarr[0]);//should return a
+//this converts array to string
+
+
+
+
+//this convert string to array
+var a = "apple,banana,orange";
+var newarr = a.split(",");
+window.alert(newarr);
